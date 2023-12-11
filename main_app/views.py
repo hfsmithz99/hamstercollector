d0841f964c674ca7c4f3635f5e96850176685b3f
Henry Smith <h.f.smithz99@gmail.com>
2023-12-11 12:27:04 -0500

    lab part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+hamsters = [
+    {'name': 'Dune', 'color': 'blonde', 'description': 'Young and full of life', 'age': 1},
+    {'name': 'Atreides', 'color': 'black', 'description': 'Quiet and gentle', 'age': 2},
+    {'name': 'Harkonnen', 'color': 'white', 'description': 'Angry. So angry.', 'age': 3},
+]
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def hamsters_index(request):
+    return render(request, 'hamsters/index.html', {'hamsters': hamsters})
