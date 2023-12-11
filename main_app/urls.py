d73534666c46f08be14aa6b2a60cf229598e1d83
Henry Smith <h.f.smithz99@gmail.com>
2023-12-11 16:37:15 -0500

    lab part 2, adding detail page and links

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
     path('hamsters/', views.hamsters_index, name='index'),
+    path('hamsters/<int:hamster_id>/', views.hamster_detail, name="detail"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-hamsters = [
-    {'name': 'Dune', 'color': 'blonde', 'description': 'Young and full of life', 'age': 1},
-    {'name': 'Atreides', 'color': 'black', 'description': 'Quiet and gentle', 'age': 2},
-    {'name': 'Harkonnen', 'color': 'white', 'description': 'Angry. So angry.', 'age': 3},
-]
+from .models import Hamster
+# hamsters = [
+  #  {'name': 'Dune', 'color': 'blonde', 'description': 'Young and full of life', 'age': 1},
+   # {'name': 'Atreides', 'color': 'black', 'description': 'Quiet and gentle', 'age': 2},
+    #{'name': 'Harkonnen', 'color': 'white', 'description': 'Angry. So angry.', 'age': 3},
+#]
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -12,4 +13,9 @@ def about(request):
     return render(request, 'about.html')
 
 def hamsters_index(request):
+    hamsters = Hamster.objects.all()
     return render(request, 'hamsters/index.html', {'hamsters': hamsters})
+
+def hamster_detail(request, hamster_id):
+    hamster = Hamster.objects.get(id=hamster_id)
+    return render(request, 'hamsters/detail.html', {'hamster': hamster})
